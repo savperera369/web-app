a676fcc1efe27aa50b3427d85a7f31dc7428db1c
saviruperera <saviru.perera@mail.mcgill.ca>
2025-03-03 17:52:25 -0500

    change width

--- src/components/CurrencyListItem.jsx
+++ src/components/CurrencyListItem.jsx
@@ -2,7 +2,7 @@
 
 const CurrencyListItem = ({ currency }) => {
     return (
-        <div className="w-1/2 mt-2 h-full border border-gray-700 p-3 rounded-lg bg-gray-500 flex items-center justify-between">
+        <div className="w-2/3 mt-2 h-full border border-gray-700 p-3 rounded-lg bg-gray-500 flex items-center justify-between">
             <p className="font-semibold text-xl text-white">{currency.name}</p>
             <p className="font-semibold text-xl text-white">{currency.price}</p>
         </div>
